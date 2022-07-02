2f124d1d92456b1057ffc0536b991f38037fe289
Khupse <88432842+Khupse@users.noreply.github.com>
2022-07-02 10:55:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function oreload(){
+}
+
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    }
+
+    function draw(){
+
+    }
+
+
+function take_snapshot(){
+save ('myFilterImage.png');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
